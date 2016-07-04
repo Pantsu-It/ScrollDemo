4c6274c1b5e63766128c9d06e59966462de79a01
Pantsu-It <479263213@qq.com>
2016-07-04 10:35:49 +0800

    commit

--- app/src/main/java/pantsu/scrolldemo/scroll_7/Activity_7.java
+++ app/src/main/java/pantsu/scrolldemo/scroll_7/Activity_7.java
@@ -2,6 +2,7 @@ package pantsu.scrolldemo.scroll_7;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -25,26 +26,28 @@ public class Activity_7 extends Activity {
     }
 
     private void configureLayout() {
-        int width = MainActivity.displayMetrics.widthPixels;
-        int height = MainActivity.displayMetrics.heightPixels - MainActivity.statusBarHeight;
-
+//        int width = MainActivity.displayMetrics.widthPixels;
+//        int height = MainActivity.displayMetrics.heightPixels - MainActivity.statusBarHeight;
 
         LinearLayout layout = new LinearLayout(this);
-        layout.setGravity(LinearLayout.VERTICAL);
+        layout.setOrientation(LinearLayout.VERTICAL);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, 100);
         for (int i = 1; i <= 20; i++) {
             Button btn = new Button(this);
             btn.setText("我是Button-" + i);
-            layout.addView(btn, params);
+            layout.addView(btn, i - 1, params);
         }
 
         OverScrollView overScrollView = new OverScrollView(this);
-        ScrollView.LayoutParams params1 = new ScrollView.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         overScrollView.setBackgroundColor(0xff445566);
-        overScrollView.addView(layout, params1);
-        setContentView(overScrollView);
+        ScrollView.LayoutParams params2 = new ScrollView.LayoutParams(
+                ScrollView.LayoutParams.MATCH_PARENT, ScrollView.LayoutParams.WRAP_CONTENT);
+        overScrollView.addView(layout, params2);
+
+        ScrollView.LayoutParams params1 = new ScrollView.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        setContentView(overScrollView, params1);
     }
 
 }
