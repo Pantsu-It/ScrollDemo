6558aa86620efb30372f38f016261cba5c5531cd
Pantsu-It <479263213@qq.com>
2016-07-13 10:30:08 +0800

    commit activity_7

--- app/src/main/java/pantsu/scrolldemo/scroll_7/Activity_7.java
+++ app/src/main/java/pantsu/scrolldemo/scroll_7/Activity_7.java
@@ -1,106 +1,249 @@
 package pantsu.scrolldemo.scroll_7;
 
 import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.LinearInterpolator;
-import android.widget.Button;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import pantsu.scrolldemo.MainActivity;
-import pantsu.scrolldemo.scroll_6.DragBackGroup;
+
+import java.lang.reflect.Field;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import pantsu.scrolldemo.R;
+import pantsu.scrolldemo.scroll_7_parts.CampaignInfo;
+import pantsu.scrolldemo.scroll_7_parts.ImageLoader;
 import pantsu.scrolldemo.scroll_container.OverScrollView;
+import pantsu.scrolldemo.scroll_7_parts.ImageUtil;
+import pantsu.scrolldemo.scroll_7_parts.SystemBarTintManager;
 
 /**
- * Created by Pantsu on 2016/3/22.
- * <p/>
- * Blog -> http://pantsu-it.github.io/
+ * Created by Pants on 2016/7/5.
  */
 public class Activity_7 extends Activity {
+    private OverScrollView overScrollView;
+    private ImageView titleBackground;
+    private LinearLayout imageScrollView;
+    private RelativeLayout.LayoutParams scrollLayoutParams;
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        configureLayout();
-    }
+        setContentView(R.layout.activity_campaign_detail);
+        setStatusBarAlpha();
+        configureTitleLayout();
+        configureImageScroll();
 
-    private void configureLayout() {
-//        int width = MainActivity.displayMetrics.widthPixels;
-//        int height = MainActivity.displayMetrics.heightPixels - MainActivity.statusBarHeight;
 
-        final LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 100);
-        for (int i = 1; i <= 20; i++) {
-            Button btn = new Button(this);
-            btn.setText("我是Button-" + i);
-            layout.addView(btn, i - 1, params);
-        }
+        ((TextView) findViewById(R.id.tvCampaignName)).setText("活动名称");
+        ((TextView) findViewById(R.id.tvCampaignTime))
+                .setText(getInterval("20160713101010", "20160729101010"));
+        ((TextView) findViewById(R.id.tvCampaignDetail)).setText("活动详情"
+                +"\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>\n<br/>" +
+                "\n" +
+                "<br/>\n" +
+                "<br/> \n" +
+                "<br/>\n" +
+                "<br/>\n" +
+                "<br/>\n" +
+                "<br/>\n" +
+                "<br/>\n" +
+                "<br/>\n" +
+                "<br/>\n" +
+                "<br/>");
+        ImageLoader.getInstance(this).bindBitmap("",
+                R.drawable.default_user_icon, (ImageView) findViewById(R.id.ivCampaignImage),new ImageLoader.BindStrategy() {
+                    @Override
+                    public void bindBitmapToTarget(ImageView imageView, Bitmap bitmap) {
+                        imageView.setImageBitmap(bitmap);
 
-        final OverScrollView overScrollView = new OverScrollView(this);
-        overScrollView.setBackgroundColor(0xff445566);
-        ScrollView.LayoutParams params2 = new ScrollView.LayoutParams(
-                ScrollView.LayoutParams.MATCH_PARENT, ScrollView.LayoutParams.WRAP_CONTENT);
-        overScrollView.addView(layout, params2);
+                        int ivWidth = getResources().getDisplayMetrics().widthPixels;
+                        int ivHeight = imageHeight;
+                        int bWidth = bitmap.getWidth();
+                        int bHeight = bitmap.getHeight();
+                        float ratioWidth = 1f * ivWidth / bWidth;
+                        float ratioHeight = 1f * ivHeight / bHeight;
+                        float ratioClipHeight = 1f * titleLayoutHeight / ivHeight;
 
-        ScrollView.LayoutParams params1 = new ScrollView.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-        setContentView(overScrollView, params1);
+                        Bitmap titleBitmap;
+                        int x, y, width, height;
+                        if (ratioWidth < ratioHeight) {
+                            width = (int) (ivWidth / ratioHeight);
+                            x = (bWidth - width) / 2;
+                            height = (int) (bHeight * ratioClipHeight);
+                            y = bHeight - height;
+                        } else {
+                            width = bWidth;
+                            x = 0;
+                            height = (int) (ivHeight / ratioWidth * ratioClipHeight);
+                            y = (int) ((bHeight + ivHeight / ratioWidth) / 2) - height;
+                        }
 
-        OverScrollView.OverScrollStrategy scrollStrategy = new OverScrollView.OverScrollStrategy() {
-            private float scrollRate = 0.5f;
-
+                        titleBitmap = Bitmap.createBitmap(bitmap, x, y, width, height);
+                        titleBitmap = ImageUtil.createDarkBitmap(titleBitmap);
+                        titleBackground.setImageBitmap(titleBitmap);
+                    }
+                });
+        overScrollView = (OverScrollView) findViewById(R.id.overScrollView);
+        overScrollView.setScrollStrategy(new OverScrollView.OverScrollStrategy() {
             @Override
-            public int onScrollTop(int y, int dy) {
-                int scrollY = Math.min(y, 200);
-                layout.scrollTo(0, -scrollY);
+            public int onOverScrollTop(int y, int dy) {
+                int scrollY = Math.min(y, imageHeight - imageScrollHeight);
+                imageScrollView.scrollTo(0, (imageHeight - imageScrollHeight - scrollY) / 2);
+                scrollLayoutParams.height = imageScrollHeight + scrollY;
+                imageScrollView.requestLayout();
                 return scrollY;
             }
 
             @Override
-            public int onScrollBottom(int y, int dy) {
-//                int scrollY = Math.min(y, 200);
-//                layout.scrollTo(0, scrollY);
-//                return scrollY;
-                return 0;
-            }
-
-            @Override
             public void onReleaseTop(int y) {
-                ValueAnimator animator = ValueAnimator.ofInt(-y, 0);
-                animator.setInterpolator(new LinearInterpolator());
-                animator.setDuration(150);
-                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                    @Override
-                    public void onAnimationUpdate(ValueAnimator animation) {
-                        int scrollY = (int) animation.getAnimatedValue();
-                        layout.scrollTo(0, scrollY);
-                    }
-                });
-                animator.start();
-            }
-
-            @Override
-            public void onReleaseBottom(int y) {
                 ValueAnimator animator = ValueAnimator.ofInt(y, 0);
-                animator.setInterpolator(new DecelerateInterpolator(0.5f));
-                animator.setDuration(150);
                 animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                     @Override
                     public void onAnimationUpdate(ValueAnimator animation) {
                         int scrollY = (int) animation.getAnimatedValue();
-                        layout.scrollTo(0, scrollY);
+                        imageScrollView.scrollTo(0, (imageHeight - imageScrollHeight - scrollY) / 2);
+                        scrollLayoutParams.height = imageScrollHeight + scrollY;
+                        imageScrollView.requestLayout();
                     }
                 });
+                animator.setDuration(150);
                 animator.start();
             }
-        };
-        overScrollView.setScrollStrategy(scrollStrategy);
+        });
+        overScrollView.setOnScrollChangeListener(new OverScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChanged(int scrollX, int scrollY, int oldX, int oldY) {
+                float alpha = 1.0f * scrollY / (imageScrollHeight - titleLayoutHeight);
+                alpha = Math.min(alpha, 1);
+                titleBackground.setAlpha(alpha);
+            }
+        });
+    }
+
+    int imageHeight;
+    int imageScrollHeight;
+    int titleLayoutHeight;
+
+    private void configureImageScroll() {
+        imageScrollView = (LinearLayout) findViewById(R.id.imageScrollView);
+
+        imageHeight = getResources().getDisplayMetrics().widthPixels;
+        imageScrollHeight = (int) (imageHeight * 0.8f);
+
+        findViewById(R.id.ivCampaignImage).setLayoutParams(
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, imageHeight));
+        scrollLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, imageScrollHeight);
+        imageScrollView.setLayoutParams(scrollLayoutParams);
+
+        final ViewTreeObserver observer = imageScrollView.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onGlobalLayout() {
+                imageScrollView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                imageScrollView.scrollTo(0, (imageHeight - imageScrollHeight) / 2);
+            }
+        });
+    }
+
+    private void configureTitleLayout() {
+        int statusBarHeight = getStatusBarHeight();
+        int titleBarHeight = (int) (50 * getResources().getDisplayMetrics().density);
+        titleLayoutHeight = statusBarHeight + titleBarHeight;
+
+        findViewById(R.id.statusBar).setLayoutParams(
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, statusBarHeight));
+        findViewById(R.id.titleBar).setLayoutParams(
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, titleBarHeight));
+
+        titleBackground = (ImageView) findViewById(R.id.titleBackground);
+        titleBackground.setLayoutParams(
+                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, titleLayoutHeight));
+        titleBackground.setBackgroundColor(0xff404040);
+        titleBackground.setAlpha(0f);
+    }
+
+    private String getInterval(String campaignStartTime, String campaignEndTime) {
+        SimpleDateFormat formatIn = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat formatOut = new SimpleDateFormat("yyyy.MM.dd");
+
+        try {
+            Date startDatetime = formatIn.parse(campaignStartTime);
+            Date endDatetime = formatIn.parse(campaignEndTime);
+            String startDate = formatOut.format(startDatetime);
+            String endDate = formatOut.format(endDatetime);
+            if (startDate.substring(0, 3).contentEquals(endDate.substring(0, 3)))
+                return startDate.concat("-").concat(endDate.substring(5));
+            else
+                return startDate.concat("-").concat(endDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public void onClick(View view) {
+        int viewId = view.getId();
+        if (viewId == R.id.btnPrevious) {
+            finish();
+        }
+    }
+
+    private int getStatusBarHeight() {
+        int statusBarHeight = 0;
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object o = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = (Integer) field.get(o);
+            statusBarHeight = getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Rect frame = new Rect();
+            getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+            statusBarHeight = frame.top;
+        }
+        return statusBarHeight;
+    }
+
+    public void setStatusBarAlpha() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //状态栏透明 需要在创建SystemBarTintManager 之前调用。
+            setTranslucentStatus(true);
+        }
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        //使StatusBarTintView 和 actionbar的颜色保持一致，风格统一。
+        tintManager.setStatusBarTintResource(R.color.colorAlpha);
+
+    }
+
+    //    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
 }
